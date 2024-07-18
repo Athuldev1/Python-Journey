3bd2b8ba7bc9499d0d4522acc82f4fcee9bfbf09
Athulkrishnan KP <110807290+Athuldev1@users.noreply.github.com>
2024-07-18 10:38:06 +0530

    Update tip_cal.py

--- Day-1_tip_calculator/tip_cal.py
+++ Day-1_tip_calculator/tip_cal.py
@@ -1,15 +1,4 @@
-
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
-
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-# Round the result to 2 decimal places.
-final_amount = round(bill_per_person, 2)
-
-
-print(f"Each person should pay: ${final_amount}")
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
