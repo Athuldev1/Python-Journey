ad344b033e748dfd81b196a1a2fc8694ce75fdc9
ATHULKRISHNAN K P <110807290+Athuldev1@users.noreply.github.com>
2024-07-28 13:28:14 +0530

    Formatted the code

--- Day-8_caesar_cipher/main.py
+++ Day-8_caesar_cipher/main.py
@@ -2,6 +2,7 @@ from caesar_art import logo, alphabet
 
 print(logo)
 
+
 def caesar_cipher(start_text, shift_amount, cipher_direction):
     final_text = ""
     if cipher_direction == "decode":
@@ -19,24 +20,26 @@ def caesar_cipher(start_text, shift_amount, cipher_direction):
             final_text += char
     print(f"The {cipher_direction}d text is {final_text}.")
 
+
 should_continue = True
 while should_continue:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     if direction not in ["encode", "decode"]:
         print("Invalid input. Please type 'encode' or 'decode'.")
         continue
-    
+
     text = input("Type your message:\n").lower()
     try:
         shift = int(input("Type the shift number:\n"))
     except ValueError:
         print("Invalid shift number. Please enter an integer.")
         continue
-
     shift %= len(alphabet)
     caesar_cipher(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-    result = input("Type 'yes' if you want to restart or 'no' if you want to exit the program: ").lower()
+    result = input(
+        "Type 'yes' if you want to restart or 'no' if you want to exit the program: "
+    ).lower()
     if result == "no":
         should_continue = False
         print("Goodbye!")
