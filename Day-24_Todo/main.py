baaf982c75a2fd930591f519d8d80930811ab599
ATHULKRISHNAN K P <110807290+Athuldev1@users.noreply.github.com>
2024-09-13 19:51:55 +0530

    Added update, display and mark as complete funtions added

--- Day-24_Todo/main.py
+++ Day-24_Todo/main.py
@@ -1,4 +1,4 @@
-from todo import add_task, remove_task
+from todo import add_task, remove_task, update_task, display_task, mark_completed_task
 
 
 def main_menu():
@@ -17,8 +17,18 @@ def main_menu():
         add_task()
     elif choice == "2":
         remove_task()
+    elif choice == "3":
+        update_task()
+    elif choice == "4":
+        display_task()
+    elif choice == "5":
+        mark_completed_task()
+    elif choice == "6":
+        print("Exiting...")
+        exit()
     else:
         print("Invalid choice. Please try again.")
+
 
 while True:
     main_menu()
--- Day-24_Todo/todo.py
+++ Day-24_Todo/todo.py
@@ -24,3 +24,39 @@ def remove_task():
     else:
         tasks = updated_tasks
         print(f"Task '{title}' removed successfully.")
+
+
+def update_task():
+    """The function for updating the tasks"""
+    title = input("Enter the task you want to update: ")
+    for task in tasks:
+        if task["title"] == title:
+            new_description = input("Enter the new description: ")
+            task["description"] = new_description
+            new_status = input("Update your status: ")
+            print(f"Task '{title}' updated successfully.")
+            return
+    print(f"{title.capitalize()} not found")
+
+
+def display_task():
+    """Function for displaying the current tasks"""
+    if not tasks:
+        print("There are no tasks available")
+    else:
+        print("\n Current Tasks")
+        for i, task in enumerate(tasks, 1):
+            print(
+                f"{i}.Title: {task['title']}\n Description: {task['description']}\n Status: {task['status']}"
+            )
+
+
+def mark_completed_task():
+    """The function shows the the current status"""
+    title = input("Enter the task title to mark as completed: ")
+    for task in tasks:
+        if task["title"] == title:
+            task["status"] = "Completed"
+            print(f"Task '{title}' marked as completed.")
+            return
+    print(f"{title.capitalize()} not found")
