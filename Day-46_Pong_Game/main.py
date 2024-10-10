69a3fc78a47e65eaa66d803323c04eba47357f7d
ATHULKRISHNAN K P <110807290+Athuldev1@users.noreply.github.com>
2024-10-10 12:37:36 +0530

    Completed the game

--- Day-46_Pong_Game/main.py
+++ Day-46_Pong_Game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 # Screen Setup
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+score = Scoreboard()
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -22,22 +24,29 @@ screen.onkeypress(l_paddle.go_down, "s")
 
 is_game_on = True
 while is_game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     # Detecting collision between ball and wall
-    if ball.xcor() > 280 or ball.ycor() < -280:
+    if ball.ycor() > 280 or ball.ycor() < -280:
         # Ball gets bouncing
         ball.bounce_y()
 
     # Detecting collision between paddles and the ball
-    if (
-        ball.distance(r_paddle) < 50
-        and ball.xcor() > 320
-        or ball.distance(l_paddle) < 50
-        and ball.xcor() > -320
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (
+        ball.distance(l_paddle) < 50 and ball.xcor() < -320
     ):
+
         ball.bounce_x()
+
+    # Detect the ball goes out of bounds
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
 
 screen.exitonclick()
