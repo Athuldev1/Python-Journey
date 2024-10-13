b7242778736af4cdcbd74763d63a2bb927952ea7
ATHULKRISHNAN K P <110807290+Athuldev1@users.noreply.github.com>
2024-10-13 15:42:31 +0530

    Game has finished

--- Day-49_Turtle_Crossing/main.py
+++ Day-49_Turtle_Crossing/main.py
@@ -8,7 +8,33 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+cars = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    cars.create_cars()
+    cars.move_cars()
+
+    # Detect when the turtle player collides with a car and stop the game if this happens.
+    for car in cars.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y).
+    # When this happens, return the turtle to the starting position and increase the speed of the cars.
+    if player.is_turtle_finished():
+        player.goto_start()
+        cars.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
