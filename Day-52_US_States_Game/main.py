24d54ecfa5b66620a28e6c7bc4b04ed95289023f
ATHULKRISHNAN K P <110807290+Athuldev1@users.noreply.github.com>
2024-10-17 11:28:00 +0530

    US states game has been finished

---
+++ Day-52_US_States_Game/main.py
@@ -0,0 +1,41 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Quiz")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = (
+        screen.textinput(
+            title=f"{len(guessed_states)}/50", prompt="What is another state?"
+        )
+        .title()
+        .strip()
+    )
+    guessed_states.append(answer_state)
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        df = pd.DataFrame(missing_states)
+        df.to_csv("missing_states.csv")
+        exit()
+
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+    else:
+        print("Wrong!")
