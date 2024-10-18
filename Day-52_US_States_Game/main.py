9ad311b4924a48802a83d2e3deb9a11c0ed1e5a0
ATHULKRISHNAN K P <110807290+Athuldev1@users.noreply.github.com>
2024-10-18 12:04:31 +0530

    Optimized the code using List comprehension.

--- Day-52_US_States_Game/main.py
+++ Day-52_US_States_Game/main.py
@@ -22,10 +22,7 @@ while len(guessed_states) < 50:
     guessed_states.append(answer_state)
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         df = pd.DataFrame(missing_states)
         df.to_csv("missing_states.csv")
         exit()
