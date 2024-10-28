8f4a17b44624bbebb0d6b19e88cb70314432542c
ATHULKRISHNAN K P <110807290+Athuldev1@users.noreply.github.com>
2024-10-28 13:54:02 +0530

    Code has been modified

--- Day-53_NATO_Alphabet/main.py
+++ Day-53_NATO_Alphabet/main.py
@@ -4,6 +4,15 @@ data = pd.read_csv("Day-53_NATO_Alphabet\\nato_alphabet.csv")
 
 alphabet = {row.letter: row.code for index, row in data.iterrows()}
 
-user_input = input("Enter the word: ").upper()
-output_list = [alphabet[letter] for letter in user_input]
-print(output_list)
+def generate_phonetics():
+    user_input = input("Enter the word: ").upper()
+    try:
+        output_list = [alphabet[letter] for letter in user_input]
+    except KeyError:
+        print("You entered an invalid input! Only alphabetic characters are allowed.")
+        generate_phonetics()
+    else:
+        print(output_list)
+
+
+generate_phonetics()
